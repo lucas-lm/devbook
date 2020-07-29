e1ded309a1ba44571c90b3329bf5fe380169e2d8
Lucas Miranda <lucas-m@outlook.com>
2020-07-29 01:17:30 -0300

    fix: font-weight unit

--- src/components/Text/index.ts
+++ src/components/Text/index.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
 interface Props {
-  size: number
-  weight: number
-  color: string
+  size?: number
+  weight?: number
+  color?: string
 }
 
-const Text = styled.h1<Props>`
+const Text = styled.p<Props>`
   font-size: ${({ size = 1 }) => `${size}em`};
-  font-weight: ${({ weight = 400 }) => `${weight}em`};
+  font-weight: ${({ weight = 400 }) => weight};
   color: ${({ color = '#fff' }) => color};
 `
 
